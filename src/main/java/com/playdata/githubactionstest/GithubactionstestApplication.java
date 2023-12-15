d1f356aca0a259e69e19a36400584946fdb946f9
dev.rary@gmail.com <dev.rary@gmail.com>
2023-12-15 14:33:01 +0900

    스크립트 실행 테스트

---
+++ src/main/java/com/playdata/githubactionstest/GithubactionstestApplication.java
@@ -0,0 +1,13 @@
+package com.playdata.githubactionstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubactionstestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubactionstestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/playdata/githubactionstest/GithubactionstestApplicationTests.java
@@ -0,0 +1,18 @@
+package com.playdata.githubactionstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubactionstestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	void testFail(){
+		throw new RuntimeException("실패했따");
+	}
+
+}
